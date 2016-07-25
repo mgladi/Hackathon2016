db488277a01f3e46f9d032fdfb611e9a9e40728c
yaland <yaland@microsoft.com>
2016-07-25 20:25:41 +0300

    app

--- Client/ServiceTest/Hello/App.cs
+++ Client/ServiceTest/Hello/App.cs
@@ -15,10 +15,11 @@ namespace Hello
         Button searchButton = new Button { Text = "Search!", BorderColor = Color.Blue };
         ListView resultsField = new ListView();
         Button GetContentOfFirstFileButton = new Button { Text = "Get content of first file!", BorderColor = Color.Blue };
+        Label FileContent = new Label();
         public App()
         {
             searchButton.Clicked += OnSearchButtonClicked;
-
+            GetContentOfFirstFileButton.Clicked += OnDownloadFileButtonClick;
             // The root page of your application
             MainPage = new ContentPage
             {
@@ -34,7 +35,8 @@ namespace Hello
                         textField,
                         searchButton,
                         GetContentOfFirstFileButton,
-                        resultsField
+                        resultsField,
+                        FileContent
                     }
                 }
             };
@@ -44,6 +46,27 @@ namespace Hello
         {
             List<ResultDataFromAgent> results = client.SearchFileInAllDeveices(textField.Text);
             resultsField.ItemsSource = results;
+            resultsField.ItemSelected += OnResultDataFromAgentItemSelected;
+        }
+
+        private void OnResultDataFromAgentItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            resultsField.ItemsSource = (e.SelectedItem as ResultDataFromAgent).FilesMetadata;
+            resultsField.ItemSelected -= OnResultDataFromAgentItemSelected;
+            resultsField.ItemSelected += OnFileMetadataItemSelected;
+        }
+
+        private void OnFileMetadataItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            resultsField.ItemSelected -= OnResultDataFromAgentItemSelected;
+            string content = client.GetFileContent(((FileMetadata)(resultsField.SelectedItem)).FullPathAndName);
+            FileContent.Text = content;
+        }
+
+        private void OnDownloadFileButtonClick(object sender, EventArgs e)
+        {
+            string content = client.GetFileContent(((FileMetadata)(resultsField.SelectedItem)).FullPathAndName);
+            FileContent.Text = content;
         }
 
         protected override void OnStart()
