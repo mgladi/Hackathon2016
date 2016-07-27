b90d60bdc1c214e87a4b34ac4ee07797a327e9c0
yaland <yaland@microsoft.com>
2016-07-27 15:32:47 +0300

    file helper add multiple folders search

--- Client/CrossDeviceSearch/CrossDeviceSearch.UWP/FileHelper.cs
+++ Client/CrossDeviceSearch/CrossDeviceSearch.UWP/FileHelper.cs
@@ -9,6 +9,7 @@ using Xamarin.Forms;
 using Windows.Networking;
 using Windows.Networking.Connectivity;
 using System.Linq;
+using System.Reflection;
 
 [assembly: Dependency(typeof(CrossDeviceSearch.UWP.FileHelper))]
 
@@ -41,8 +42,21 @@ namespace CrossDeviceSearch.UWP
 
         private async Task<List<FileMetadata>> SearchFilesAsync(string searchPattern)
         {
-            StorageFolder picturesFolder = KnownFolders.PicturesLibrary;
-            List<FileMetadata> allFilesMetadata = await RecursiveFileSearch(picturesFolder, searchPattern);
+            List<FileMetadata> allFilesMetadata = new List<FileMetadata>();
+
+            // >>>>>> Comments are for folders that are unauthorized access)<<<<<<<<<<<<
+
+            allFilesMetadata.AddRange(await RecursiveFileSearch(KnownFolders.AppCaptures, searchPattern));
+            //allFilesMetadata.AddRange(await RecursiveFileSearch(KnownFolders.DocumentsLibrary, searchPattern)); // manifest error
+            //allFilesMetadata.AddRange(await RecursiveFileSearch(KnownFolders.HomeGroup, searchPattern));
+            //allFilesMetadata.AddRange(await RecursiveFileSearch(KnownFolders.MediaServerDevices, searchPattern));
+            allFilesMetadata.AddRange(await RecursiveFileSearch(KnownFolders.MusicLibrary, searchPattern));
+            allFilesMetadata.AddRange(await RecursiveFileSearch(KnownFolders.Objects3D, searchPattern));
+            allFilesMetadata.AddRange(await RecursiveFileSearch(KnownFolders.PicturesLibrary, searchPattern));
+            //allFilesMetadata.AddRange(await RecursiveFileSearch(KnownFolders.Playlists, searchPattern));
+            //allFilesMetadata.AddRange(await RecursiveFileSearch(KnownFolders.RecordedCalls, searchPattern));
+            //allFilesMetadata.AddRange(await RecursiveFileSearch(KnownFolders.RemovableDevices, searchPattern));
+            allFilesMetadata.AddRange(await RecursiveFileSearch(KnownFolders.VideosLibrary, searchPattern));
 
             return allFilesMetadata;
         }
@@ -63,14 +77,14 @@ namespace CrossDeviceSearch.UWP
 
         private async Task<List<FileMetadata>> GetMetadataFilesListOfFolder(StorageFolder folder, string searchPattern)
         {
-            IReadOnlyList <StorageFile> fileList = await folder.GetFilesAsync();
+            IReadOnlyList<StorageFile> fileList = await folder.GetFilesAsync();
 
             List<FileMetadata> filenames = new List<FileMetadata>();
             foreach (StorageFile file in fileList)
             {
                 if (isFileNameMatchThePattern(Path.GetFileName(file.Path), searchPattern))
                 {
-                    filenames.Add(GetFileMetaData(file.Path));
+                    filenames.Add(GetFileMetaData(file));
                 }
             }
 
@@ -83,7 +97,7 @@ namespace CrossDeviceSearch.UWP
             string fileName = string.Join(".", fileNameAndExtensionArray, 0, fileNameAndExtensionArray.Length - 1);
             string fileExtension = fileNameAndExtensionArray.Last();
 
-            if(!searchPattern.Contains("*"))
+            if (!searchPattern.Contains("*"))
             {
                 return fileName.Contains(searchPattern);
             }
@@ -109,7 +123,7 @@ namespace CrossDeviceSearch.UWP
             byte[] bytes;
             using (BinaryReader reader = new BinaryReader((await file.OpenReadAsync()).AsStreamForRead()))
             {
-                 bytes = reader.ReadBytes((int)stream.Length);
+                bytes = reader.ReadBytes((int)stream.Length);
             }
 
             return bytes;
@@ -143,15 +157,17 @@ namespace CrossDeviceSearch.UWP
             return Windows.Storage.ApplicationData.Current.LocalFolder;
         }
 
-        private FileMetadata GetFileMetaData(string filepath)
+        private FileMetadata GetFileMetaData(StorageFile file)
         {
-            //TODO: finish
+            Stream stream = file.OpenReadAsync().AsTask().Result.AsStreamForRead();
+            
             return new FileMetadata
             {
-                FullPathAndName = filepath,
-                Time = DateTime.Now, // File.GetCreationTime(filepath),
-                Size = 10 // Convert.ToInt32(new FileInfo(filepath).Length)
+                FullPathAndName = file.Path,
+                Time = file.DateCreated.Date,
+                Size = (int)stream.Length 
             };
+
         }
     }
 }
